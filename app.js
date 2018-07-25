57a64351c2c4d53940bf077a23e09816a7b4fbc2
Kathryn Parker <kparker@warpedworks.com>
2018-07-24 20:09:06 -0400

    initial

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require('dotenv');
 
-var apiRouter = require('./routes/api');
+const apiRouter = require('./routes/api');
 
-var app = express();
+dotenv.load();
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
