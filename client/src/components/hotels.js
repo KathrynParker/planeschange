7b35c896d7664f1f04602076ba389c600ac1978e
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-07-29 15:37:57 -0400

    hotels comp and route

---
+++ client/src/components/hotels.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+
+class Hotels extends Component {
+
+	render() {
+		return (
+			<div>
+                <div className='listDiv'>
+                    <h5>Hotel Name</h5>
+                    <h5>Address</h5>
+                    <h5>City</h5>
+                    <h5>State</h5>
+                    <h5>Minimum Daily Rate</h5>
+                    <h5>Phone</h5>
+                </div>
+            </div>
+			)
+	}
+}
+
+export default Hotels;
