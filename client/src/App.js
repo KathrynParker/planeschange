f210339a34f618a848c1dea598ab5a400cc6f032
Kathryn Parker <kparker@warpedworks.com>
2018-07-28 23:54:02 -0400

    added login-register to react

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/login';
 import FlightInfo from '../src/components/flightinfo/flightInfo';
 
 class App extends Component {
@@ -11,9 +12,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <Login></Login>
+        </div>
+        <hr></hr>
         <FlightInfo />
       </div>
     );
