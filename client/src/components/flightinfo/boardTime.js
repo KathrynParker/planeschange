cf91a29a7775eb2cc3617c74e77a0bd8ddb87fc8
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-08-03 21:24:05 -0400

    fixed board time

--- client/src/components/flightinfo/boardTime.js
+++ client/src/components/flightinfo/boardTime.js
@@ -7,10 +7,14 @@ function timeConverter(UNIX_timestamp){
 	let min = a.getMinutes();
 	let time;
 
-	if (hour > 12) {
-		return time = (hour - 12) + ':' + min + ' PM EDT';
-	} else if (min === 12) {
-		return time = hour + ':' + 12 + ' AM EDT';
+	if (hour > 12 && min > 40) {
+		return time = (hour - 12) + ':' + (min - 40) + ' PM EDT';
+	} else if (hour === 0 && min > 40) {
+		return time = 12 + ':' + (min - 40) + ' AM EDT';
+	} else if (hour === 0 && min < 40) {
+		return time = 12 + ':' + (min - 10) + ' AM EDT';
+	} else if (hour > 12 && min < 40) {
+		return time = (hour - 12) + ':' + (min - 10) + ' PM EDT';
 	}
 }
 
