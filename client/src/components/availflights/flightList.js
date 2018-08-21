e92f577856726ff63bc9ccb6451536f201913b9e
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-08-21 19:31:31 -0400

    fixed css for availflights

--- client/src/components/availflights/flightList.js
+++ client/src/components/availflights/flightList.js
@@ -6,16 +6,12 @@ class FlightList extends Component {
 
 	render() {
 		return (
-            <div className="container roundedCard mb-3">
-                <div className="row" >
-                    <div className="col">
-                        <h5>Departs at: {this.props.departs_at}</h5>
-                        <h5>Arrives at: {this.props.arrives_at}</h5>
-                        <h5>Price: {this.props.price}</h5>
-                        <div className='airlineDiv'>
-                            <h5>Airline: {this.props.airline}</h5>
-                        </div>
-                    </div>
+            <div className="col-4 roundedCard mb-3">
+                <h5>Departs at: {this.props.departs_at}</h5>
+                <h5>Arrives at: {this.props.arrives_at}</h5>
+                <h5>Price: ${this.props.price}</h5>
+                <div className='airlineDiv'>
+                    <h5>Airline: {this.props.airline}</h5>
                 </div>
             </div>
 			)
